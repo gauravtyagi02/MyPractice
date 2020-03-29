419f60200cda1124452eabde1aff0862bf93b4cc
Gaurav Tyagi <gauravtyagi02@gmail.com>
2020-03-29 17:30:16 +0530

    Spring intializer project commit

---
+++ src/main/java/com/git/gitdemospringInitiazer/GitDemoSpringInitiazerApplication.java
@@ -0,0 +1,13 @@
+package com.git.gitdemospringInitiazer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitDemoSpringInitiazerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitDemoSpringInitiazerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/git/gitdemospringInitiazer/GitDemoSpringInitiazerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git.gitdemospringInitiazer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitDemoSpringInitiazerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
